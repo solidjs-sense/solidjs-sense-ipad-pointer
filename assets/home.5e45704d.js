d3cf2fea1cc0f97e74cdbeda55a97b183e6de5d0
iamcco <ooiss@qq.com>
2022-05-03 16:04:53 +0800

    update

--- assets/home.5e45704d.js
+++
@@ -1,1 +0,0 @@
-import{i,t as n}from"./vendor.97cd0a63.js";import{t as r}from"./index.c3e30ef9.js";const s=n('<div class="home-ctn"><div class="content"><h1></h1><p></p></div></div>');var d=()=>(()=>{const t=s.cloneNode(!0),l=t.firstChild,e=l.firstChild,o=e.nextSibling;return i(e,()=>r("SolidJS iPad Pointer")),i(o,()=>r("implement")),t})();export{d as default};
